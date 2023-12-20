f9ab486f71dfa56660c17ef4ccd70c7575ac4f86
Alexey Efimov <alex-vngd@yandex.ru>
2023-12-20 17:19:16 +0300

    fixes

--- src/services/middleware/user-socket-middleware.js
+++ src/services/middleware/user-socket-middleware.js
@@ -34,9 +34,9 @@ export const userSocketMiddleware = (wsActions) => {
                     const parsedData = JSON.parse(data);
 
                     dispatch({ type: onMessage, payload: parsedData });
-                    if (!parsedData.message) {
+/*                     if (!parsedData.message) {
                         
-                    }
+                    } */
                 };
 
                 socket.onclose = () => {
